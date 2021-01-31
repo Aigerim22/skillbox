53c3f77650ee683bd68cc8480e7e2070a98e7b10
Aigerim Tazhibay <47142525+Aigerim22@users.noreply.github.com>
2021-01-31 22:16:44 +0600

    add

---
+++ skillbox2/serve.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+      frame: false,
+  });
+  win.loadFile('dist/index.html');
+  // win.loadURL("http://localhost:1234")
+  // win.webContents.openDevTools();
+});
+
