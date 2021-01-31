78fec8713cef3c57b3ba9af9325f81b3bc3b25f7
Aigerim22 <aika0822t@gmail.com>
2021-01-31 23:26:08 +0600

    3 files modified

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const isElectron = navigator.userAgent.indexOf('Electron') !== -1;
-function changeContainer() {
+import "./style.css";
+const isElectron = navigator.userAgent.indexOf("Electron") !== -1;
+// function changeContainer() {
+//   const containerActive = document.querySelector(".js-container.is-active");
+//   const containerInactive = document.querySelector(
+//     ".js-container:not(.is-active)"
+//   );
+
+//   containerActive.classList.remove("is-active");
+//   containerInactive.classList.add("is-active");
+// }
+const button2 = document.getElementById("code");
+function go() {
   const containerActive = document.querySelector(".js-container.is-active");
   const containerInactive = document.querySelector(
     ".js-container:not(.is-active)"
   );
-
   containerActive.classList.remove("is-active");
   containerInactive.classList.add("is-active");
 }
+
+button2.onclick = go;
+
 function resetApp() {
   inputTicket.value = "";
 }
@@ -16,15 +29,15 @@ const form = document.getElementById("form");
 
 const inputTicket = document.getElementById("inputTicket");
 
-form.onsubmit = (event) => {
+form.onsubmit = event => {
   event.preventDefault();
-  changeContainer();
+  // changeContainer();
   setTimeout(resetApp, 463);
 };
 
 const resetBtn = document.getElementById("reset");
-resetBtn.onclick = changeContainer;
+// resetBtn.onclick = changeContainer;
 if (isElectron) {
-  document.body.classList.add('electron');
-  alert('halo marsaine!!!')
+  document.body.classList.add("electron");
+  alert("halo marsaine!!!");
 }
