63d7f7bff9681bc36407cfe9b06296b3840c9f5a
Aigerim Tazhibay <47142525+Aigerim22@users.noreply.github.com>
2021-01-31 22:18:03 +0600

    init

---
+++ skillbox2/src/script.js
@@ -0,0 +1,30 @@
+const isElectron = navigator.userAgent.indexOf('Electron') !== -1;
+function changeContainer() {
+  const containerActive = document.querySelector(".js-container.is-active");
+  const containerInactive = document.querySelector(
+    ".js-container:not(.is-active)"
+  );
+
+  containerActive.classList.remove("is-active");
+  containerInactive.classList.add("is-active");
+}
+function resetApp() {
+  inputTicket.value = "";
+}
+
+const form = document.getElementById("form");
+
+const inputTicket = document.getElementById("inputTicket");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  changeContainer();
+  setTimeout(resetApp, 463);
+};
+
+const resetBtn = document.getElementById("reset");
+resetBtn.onclick = changeContainer;
+if (isElectron) {
+  document.body.classList.add('electron');
+  alert('halo marsaine!!!')
+}
