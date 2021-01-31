3177f31953d5b5f96472e45f3cf2bdee448d652a
Aigerim Tazhibay <47142525+Aigerim22@users.noreply.github.com>
2021-01-31 23:14:44 +0600

    Update script.js

--- skillbox2/src/script.js
+++ skillbox2/src/script.js
@@ -1,13 +1,25 @@
 const isElectron = navigator.userAgent.indexOf('Electron') !== -1;
-function changeContainer() {
+// function changeContainer() {
+//   const containerActive = document.querySelector(".js-container.is-active");
+//   const containerInactive = document.querySelector(
+//     ".js-container:not(.is-active)"
+//   );
+
+//   containerActive.classList.remove("is-active");
+//   containerInactive.classList.add("is-active");
+// }
+const button2 = document.getElementById('code');
+function go(){
   const containerActive = document.querySelector(".js-container.is-active");
   const containerInactive = document.querySelector(
     ".js-container:not(.is-active)"
   );
-
   containerActive.classList.remove("is-active");
   containerInactive.classList.add("is-active");
 }
+
+button2.onclick=go;
+
 function resetApp() {
   inputTicket.value = "";
 }
@@ -18,12 +30,12 @@ const inputTicket = document.getElementById("inputTicket");
 
 form.onsubmit = (event) => {
   event.preventDefault();
-  changeContainer();
+  // changeContainer();
   setTimeout(resetApp, 463);
 };
 
 const resetBtn = document.getElementById("reset");
-resetBtn.onclick = changeContainer;
+// resetBtn.onclick = changeContainer;
 if (isElectron) {
   document.body.classList.add('electron');
   alert('halo marsaine!!!')
